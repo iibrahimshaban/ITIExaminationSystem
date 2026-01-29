336000e5fefc2bcde9548f645404b50cfefc1c23
ibrahim khaled <ibrahim khaled@DESKTOP-GESLO5P>
2026-01-29 18:00:21 +0200

    Add Instructor Service SP Plus Configuration

---
+++ ExaminationSystem/Contracts/CourseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ExaminationSystem.Contracts;
+
+public record CourseDto(
+    int InstructorId,
+    string InstructorName,
+    string CourseCode,
+    string CourseTitle,
+    string ExamTitle,
+    int DurationInMinutes,
+    int TotalQuestions,
+    int TF_Count,
+    int MCQ_Count
+    );
---
+++ ExaminationSystem/Services/InstructorService.cs
@@ -0,0 +1,41 @@
+﻿using ExaminationSystem.Abstractions.ResultPattern;
+using ExaminationSystem.Errors;
+using ExaminationSystem.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace ExaminationSystem.Services;
+
+public class InstructorService(ApplicationDbContext context) : IInstructorServcie
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<Result<IEnumerable<CouresExamForInstructor>>> FindAvilabeExamsAsync(string InstructorId, CancellationToken cancellationToken)
+    {
+        var result = await _context.CouresExamForInstructors.
+            FromSql($"EXEC FindAvilableCourseAndExams @UserId={InstructorId}").ToListAsync(cancellationToken);
+
+        if (result.Count < 1)
+            return Result.Failure<IEnumerable<CouresExamForInstructor>>(InstructorErrors.NoExamsFound);
+
+        return Result.Success<IEnumerable<CouresExamForInstructor>>(result);
+    }
+
+    public async Task<Result> GenerateAndAssignRandomExamAsync(int ExamId, int NumberOfMCQ, int NumberOfTrueFalse, int MaxStudnet = 20, CancellationToken cancellationToken = default)
+    {
+        var affectedRows = await _context.Database.ExecuteSqlRawAsync(
+            "EXEC AssignRandomQuestionsToAllStudents @ExamId, @MCQCount, @TrueFalseCount, @MaxStudents",
+            new SqlParameter("@ExamId", ExamId),
+            new SqlParameter("@MCQCount", NumberOfMCQ),
+            new SqlParameter("@TrueFalseCount", NumberOfTrueFalse),
+            new SqlParameter("@MaxStudents", MaxStudnet)
+        );
+
+        // You may want to return a Result based on affectedRows, e.g.:
+        return affectedRows > 0
+            ? Result.Success()
+            : Result.Failure(InstructorErrors.InvalidSp);
+    }
+}
