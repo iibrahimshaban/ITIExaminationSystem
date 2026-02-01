500465cc8a5550d8c71adeea828cc77daa1e2b81
ITI-Student <student@iti.gov.eg>
2026-02-02 00:32:31 +0200

    "Create Exam Module"

---
+++ ExaminationSystem/ViewModel/CreateExamVm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExaminationSystem.ViewModel
+{
+    public class CreateExamVm
+    {
+        public int CourseId { get; set; }
+
+        public string CourseTitle { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Range(10, 300)]
+        public int DurationInMinutes { get; set; }
+
+        [Range(1, 500)]
+        public int TotalPoints { get; set; }
+
+        public bool IsRandomized { get; set; }
+
+     
+    }
+}
